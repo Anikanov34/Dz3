9cea158542201adc14d2895ef2ded881b39e1a16
Anikanov <ivanov@mail.ru>
2022-09-27 23:44:20 +0300

    Programs.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,43 @@
-﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 23432 -> да
-// 12821 -> да
-Console.WriteLine("Задача 19");
-Console.Write("Введите число: ");
-void CheckingNumber() 
+﻿
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// Console.WriteLine("\n Задача 21");
+// int x1 = Coordinate("x", "A");
+// int y1 = Coordinate("y", "A");
+// int z1 = Coordinate("z", "A");
+// int x2 = Coordinate("x", "B");
+// int y2 = Coordinate("y", "B");
+// int z2 = Coordinate("z", "B");
+
+// int Coordinate(string coorName, string pointName)
+// {
+//     Console.Write($"Введите координату {coorName} точки {pointName}: ");
+//     return Convert.ToInt16(Console.ReadLine());
+// }
+
+// double Decision(double x1, double x2, 
+//                 double y1, double y2, 
+//                 double z1, double z2){
+//   return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+//                    Math.Pow((y2-y1), 2) + 
+//                    Math.Pow((z2-z1), 2));
+// }
+
+// double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+// Console.WriteLine($"Длина отрезка  {segmentLength}");
+void CheckingNumber()
 {
-    int number = 23432;
-    int current_number = number; 
-    int result = 0;
-    while (current_number > 0)
-    {
-        int digit = current_number % 10;
-        result = result * 10 + digit;
-        current_number  /= 10;
-        Console.WriteLine(result);
-    }
-    if (number == result)
-    {
-        Console.WriteLine($"число {number} палиндром");
-    }
-    else
-    {
-        Console.WriteLine($"число {number} не палиндром");
-    }
-   
-    
+int a_x = 1;
+int a_y = 1;
+int a_z = 1;
+int b_x = 0;
+int b_y = 0;
+int b_z = 0;
+
+double result = Math.Sqrt(Math.Pow(a_x-b_x, 2)+ Math.Pow(a_y-b_y, 2)+ Math.Pow(a_z-b_z, 2));
+Console.WriteLine(result);
 }
- CheckingNumber();
+CheckingNumber();
